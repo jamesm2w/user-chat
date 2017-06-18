2b1ecc311d25851aeffa6ee4461d97d5261d470a
System <none>
2017-06-18 10:13:00 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,40 +6,12 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const _ = require('lodash');
+const UUID = require("node-uuid");
 
-let autoId = 0;
-let players = [];
+var gameport = process.env.port || 4004;
+
+console.log("Listening...")
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-
-io.on('connection', (client) => {
-  let player;
-  let id;
-
-  client.on('auth', (opts, cb) => {
-    id = ++autoId;
-    player = id;
-    players.push(player);
-    // Callback with id
-    cb({ id: autoId });
-  });
-  
-  // Receive keystrokes
-  client.on('key', (key) => {
-    // and change direction accordingly
-    if(player) {
-      player.changeDirection(key);
-    }
-  });
-
-  // Remove players on disconnect
-  client.on('disconnect', () => {
-    _.remove(players, player);
-  });
 });
